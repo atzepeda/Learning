4ae66a95d53d5dba878c5e1eb2e3da7fe09cf9a0
atzepeda <atzepeda27@gmail.com>
2020-12-12 20:28:15 -0500

    just trying to run this

---
+++ ufcEventBriefing/ufcEventBriefing/spiders/event.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy_splash import SplashRequest
+
+
+class EventSpider(scrapy.Spider):
+    name = 'event'
+    allowed_domains = ['www.google.com']
+    
+    script = '''
+        function main(splash, args)
+            url = args.url
+            assert(splash:go(url))
+            assert(splash:wait(1))
+
+            input_box = assert(splash:select(".gLFyf.gsfi"))
+            input_box:focus()
+            input_box:send_text("ufc")
+            assert(splash:wait(5))
+
+            btn = assert(splash:select(".gNO89b"))
+            btn:mouse_click()
+            assert(splash:wait(5))
+
+            splash:set_viewport_full()
+
+            return splash:html()
+        end
+    '''
+
+    def start_requests(self):
+        yield SplashRequest(url="www.google.com", callback=self.parse, endpoint="execute", args={
+            'lua_source': self.script
+        })
+
+    def parse(self, response):
+        print(response.body)
