e6a028922f3762dd66980fd05033793b9306b25d
atzepeda <51332489+atzepeda@users.noreply.github.com>
2020-12-13 01:16:34 -0500

    Im finished for the night

---
+++ dateTest.py
@@ -0,0 +1,11 @@
+import datetime
+
+'''
+currentDate = "Sat, Dec 5, 10:00 PM"
+newDate = currentDate.split(',', 1)
+#print(newDate[1])
+finalDate = datetime.datetime.strptime(currentDate, '%a, %b %d, %I:%M %p')
+print(datetime.datetime.now())
+print(finalDate)
+print((datetime.datetime.now() - finalDate).total_seconds())
+'''
--- ufcEventBriefing/ufcEventBriefing/spiders/event.py
+++ ufcEventBriefing/ufcEventBriefing/spiders/event.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapy_splash import SplashRequest
+import datetime
 
 
 class EventSpider(scrapy.Spider):
@@ -17,11 +18,11 @@ class EventSpider(scrapy.Spider):
             input_box = assert(splash:select(".gLFyf.gsfi"))
             input_box:focus()
             input_box:send_text("ufc")
-            assert(splash:wait(5))
+            assert(splash:wait(1))
 
             btn = assert(splash:select(".gNO89b"))
             btn:mouse_click()
-            assert(splash:wait(5))
+            assert(splash:wait(1))
 
             splash:set_viewport_full()
 
@@ -31,9 +32,28 @@ class EventSpider(scrapy.Spider):
 
     def start_requests(self):
         yield SplashRequest(url="https://www.google.com", callback=self.parse, endpoint="execute", args={
-            'lua_source': self.script
+            'lua_source': self.script,
         })
 
     def parse(self, response):
-        print(response.body)
+        cardDate = response.xpath("//div[@class='tsp-cpd tsp-rpd tsp-flr']//span[@class='tsp-cp']/text()").get()
+        #print(cardDate.split()[0])
+        #print(cardDate)
+        newDate = cardDate.split(",")
+        #print("*************************************")
+        #print(newDate[0])
+        if (newDate[0]=="today" or newDate[0]=="yesterday"):
+            print("THIS TRIGGERED*******************")
+        else:
+            dateObj = datetime.datetime.strptime(cardDate, '%a, %b %d, %I:%M %p')
+            if(datetime.datetime.now() - dateObj).total_seconds() > 0:
+                print("OPTION A")
+                yield {
+                    'date': dateObj
+                }
+            else:
+                print("OPTION B")
+                yield {
+                    'date': cardDate
+                }
 
