d1f17a83ce9e5f1c618bdfec0857a8b1a8acf66a
atzepeda <51332489+atzepeda@users.noreply.github.com>
2020-12-13 23:45:10 -0500

    a whole mess of stuff right now

--- ufcEventBriefing/ufcEventBriefing/spiders/event.py
+++ ufcEventBriefing/ufcEventBriefing/spiders/event.py
@@ -1,28 +1,32 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from twisted.internet import reactor, defer
 from scrapy_splash import SplashRequest
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.log import configure_logging
 import datetime
 
+nextCard = ''
 
 class EventSpider(scrapy.Spider):
     name = 'event'
     allowed_domains = ['www.google.com']
     
-    script = '''
+    eventScript = '''
         function main(splash, args)
             splash.private_mode_enabled = false
             url = args.url
             assert(splash:go(url))
-            assert(splash:wait(1))
+            assert(splash:wait(5))
 
             input_box = assert(splash:select(".gLFyf.gsfi"))
             input_box:focus()
             input_box:send_text("ufc")
-            assert(splash:wait(1))
+            assert(splash:wait(5))
 
             btn = assert(splash:select(".gNO89b"))
             btn:mouse_click()
-            assert(splash:wait(1))
+            assert(splash:wait(5))
 
             splash:set_viewport_full()
 
@@ -32,28 +36,73 @@ class EventSpider(scrapy.Spider):
 
     def start_requests(self):
         yield SplashRequest(url="https://www.google.com", callback=self.parse, endpoint="execute", args={
-            'lua_source': self.script,
+            'lua_source': self.eventScript,
+            'wait': 5,
         })
 
     def parse(self, response):
         cardDate = response.xpath("//div[@class='tsp-cpd tsp-rpd tsp-flr']//span[@class='tsp-cp']/text()").get()
-        #print(cardDate.split()[0])
-        #print(cardDate)
         newDate = cardDate.split(",")
-        #print("*************************************")
-        #print(newDate[0])
+
         if (newDate[0]=="today" or newDate[0]=="yesterday"):
-            print("THIS TRIGGERED*******************")
+            nextCard = response.xpath("//a[@tabindex='0']/following-sibling::a[1]/div/span/div/span/span/text()").get()
         else:
             dateObj = datetime.datetime.strptime(cardDate, '%a, %b %d, %I:%M %p')
             if(datetime.datetime.now() - dateObj).total_seconds() > 0:
-                print("OPTION A")
-                yield {
-                    'date': dateObj
-                }
+                print((datetime.datetime.now() - dateObj).total_seconds())
+                nextCard = response.xpath("//a[@tabindex='0']/text()").get()
+
             else:
-                print("OPTION B")
-                yield {
-                    'date': cardDate
-                }
+                print((datetime.datetime.now() - dateObj).total_seconds())
+                nextCard = response.xpath("//a[@tabindex='0']/following-sibling::a[1]/div/span/div/span/span/text()").get()
+
+class fighterSpider(scrapy.Spider):
+    name = 'event'
+    allowed_domains = ['www.tapology.com']
+
+    fighterScript = '''
+        function main(splash, args)
+            splash.private_mode_enabled = false
+            assert(splash:go(args.url))
+            assert(splash:wait(5))
+            
+            input_box = assert(splash:select("#siteSearch"))
+            input_box:focus()
+            input_box:send_text(splash.event)
+            assert(splash:wait(5))
+            
+            btn = assert(splash:select("#search"))
+            btn:mouse_click()
+            assert(splash:wait(5))
+            
+            splash:set_viewport_full()
+            
+            return splash:html()
+        end
+    '''
+
+    def start_requests(self):
+        yield SplashRequest(url="https://www.tapology.com", callback=self.parse, endpoint="execute", args={
+            'lua_source': self.fighterScript,
+            'wait': 5,
+            'event': nextCard
+        })
+    
+    def parse(self, response):
+        event = response.xpath("//div[@class='searchResultsEvent']/table/tbody/tr[1]/text()").get()
+        print(event)
+        print("**********************************************")
+
+configure_logging()
+runnerOne = CrawlerRunner()
+runnerTwo = CrawlerRunner()
+
+@defer.inlineCallbacks
+def crawl():
+    yield runnerOne.crawl(EventSpider)
+    yield runnerTwo.crawl(fighterSpider)
+    reactor.stop()
+
+crawl()
+reactor.run()
 
